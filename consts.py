d76635de63876a8317e61b2edddc8aedae5bc968
sc4321 <shlomocohen2@gmail.com>
2024-06-18 07:55:18 +0300

    Improve date text black backgrount

--- consts.py
+++ consts.py
@@ -6,6 +6,6 @@ C_bottomLeftCornerOfText_small = (5, 100)
 C_fontScale = 1.3
 C_fontColor = (255, 255, 255)
 C_fontColor_black = (0, 0, 0)
-C_thickness_8 = 8
+C_thickness_8 = 12
 C_thickness_4 = 4
 C_lineType = 2
